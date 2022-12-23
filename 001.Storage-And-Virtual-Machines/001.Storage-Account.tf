159ff81510e49a45c3a5d0ae1d7a499beca8415e
raine2703 <118910194+raine2703@users.noreply.github.com>
2022-12-24 00:43:23 +0200

    Update 001.Storage-Account.tf

--- 001.Storage-And-Virtual-Machines/001.Storage-Account.tf
+++ 001.Storage-And-Virtual-Machines/001.Storage-Account.tf
@@ -1,1 +1,36 @@
-// Creating Storage ACC
+//terraform plan -out main.tfplan
+//terraform apply
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "d030343c-fdd7-47cb-a6b7-b7027471025d"
+  tenant_id = "c20a49b8-2914-4624-a197-1e882bd3abf4"
+  client_id = "8498fe17-629b-4ad3-9d14-a6c2c37b90a9"
+  client_secret = "Yrr8Q~dHXo1wZZV-sl_gvzt61ZvxEpeIOd8.Gdo_"
+  features {}
+}
+
+
+
+
+resource "azurerm_resource_group" "RG" {
+  name     = "RnRG32"
+  location = "North Europe"
+}
+
+resource "azurerm_storage_account" "rnstorageacc2703x52" {
+  name                     = "rnstorageacc2703x52"
+  resource_group_name      = azurerm_resource_group.RG.name
+  location                 = azurerm_resource_group.RG.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind = "StorageV2"
+}
