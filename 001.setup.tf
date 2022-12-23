96caa25572927b21d0899991f5d2fae50c399c83
raine2703 <118910194+raine2703@users.noreply.github.com>
2022-12-24 00:01:48 +0200

    Update and rename 100.txt to 001.setup.tf

---
+++ 001.setup.tf
@@ -0,0 +1,36 @@
+//Terraform Set Up - Download Terraform, Add Environment variable in Windows, Choose provider and Start using Terraform!
+//Terraform as application must be registered in AAD. Also it must be granted necessary RBAC permissions on Subscription level. Contributor as example.
+
+//terraform -version
+//terraform plan -out main.tfplan
+//terraform apply
+
+
+//Defining that i will be using Azure
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.37.0"
+    }
+  }
+}
+
+//Authenticating to Azure
+provider "azurerm" {
+  subscription_id = "d030343c-fdd7-47cb-a6b7-b7027471025d"
+  tenant_id = "c20a49b8-2914-4624-a197-1e882bd3abf4"
+  client_id = "8498fe17-629b-4ad3-9d14-a6c2c37b90a9"
+  client_secret = "Yrr8Q~dHXo1wZZV-sl_gvzt61ZvxEpeIOd8.Gdo_"
+  features {}
+}
+
+//Thats it! :)
+resource "azurerm_resource_group" "example" {
+  name     = "RnRG32"
+  location = "North Europe"
+}
+
+//Deploy to Azure with these simple comands:
+//terraform plan -out main.tfplan
+//terraform apply
