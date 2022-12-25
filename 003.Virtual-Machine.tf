9d2a923f7976fc26b843f68e677f008f99b1a542
raine2703 <118910194+raine2703@users.noreply.github.com>
2022-12-25 03:11:53 +0200

    Update 003.Virtual-Machine.tf

--- 003.Virtual-Machine.tf
+++ 003.Virtual-Machine.tf
@@ -20,32 +20,31 @@ provider "azurerm" {
   features {}
 }
 
-
 //Defining local variables
 locals {
-  resource_group_name = "RG3"
+  resource_group_name = "RG"
   location = "North Europe"
-  NIC_name = "VM1NIC"
-  NSG_name = "NSG1"
-  VM_name = "VM1"
-  VM_size = "Standard_B2s"
-  VM_storage_account_type = "Standard_LRS"
-  VM_sku = "2022-Datacenter"
-  VM_username = "adminuser"
-  VM_password = "P@$$w0rd1234!"
+  nic_name = "vm1nic"
+  nsg_name = "nsg"
+  vm_name = "vm1"
+  vm_size = "Standard_B2s"
+  vm_storage_account_type = "Standard_LRS"
+  vm_sku = "2022-Datacenter"
+  vm_username = "adminuser"
+  vm_password = "P@$$w0rd1234!"
 //map (object) variables
   virtual_network = {
-    name = "Vnet1"
+    name = "vnet"
     adress_space = "10.0.0.0/16"
   }
 //list variables
   subnets = [
     {
-      name = "SubnetA"
+      name = "production"
       address_range = "10.0.1.0/24"
     },
     {
-      name = "SubnetB"
+      name = "test"
       address_range = "10.0.2.0/24"
     }
   ]
@@ -53,20 +52,19 @@ locals {
 
 
 //Creating Resource Group
-resource "azurerm_resource_group" "RG3" {
+resource "azurerm_resource_group" "resource-group" {
   name     = local.resource_group_name
   location = local.location
 }
 
-
 //Creating Virtual Network
-resource "azurerm_virtual_network" "Vnet1" {
+resource "azurerm_virtual_network" "virtual-network" {
   name                = local.virtual_network.name
   address_space       = [local.virtual_network.adress_space]
   location            = local.location
   resource_group_name = local.resource_group_name
   depends_on = [
-    azurerm_resource_group.RG3
+    azurerm_resource_group.resource-group
   ]
 }
 
@@ -77,7 +75,7 @@ resource "azurerm_subnet" "SubnetA" {
   virtual_network_name = local.virtual_network.name
   address_prefixes     = [local.subnets[0].address_range]
   depends_on = [
-    azurerm_virtual_network.Vnet1
+    azurerm_virtual_network.virtual-network
   ]
 }
 
@@ -88,24 +86,24 @@ resource "azurerm_subnet" "SubnetB" {
   virtual_network_name = local.virtual_network.name
   address_prefixes     = [local.subnets[1].address_range]
   depends_on = [
-    azurerm_virtual_network.Vnet1
+    azurerm_virtual_network.virtual-network
   ]
 }
 
 //Creating Public IP
 resource "azurerm_public_ip" "Public-IP" {
-  name                = "Vm1-Public-IP"
+  name                = "${local.vm_name}-publicip"
   resource_group_name = local.resource_group_name
   location            = local.location
   allocation_method   = "Static"
   depends_on = [
-    azurerm_resource_group.RG3
+    azurerm_resource_group.resource-group
   ]
 }
 
 //Creating NIC
-resource "azurerm_network_interface" "VM1NIC" {
-  name                = local.NIC_name
+resource "azurerm_network_interface" "vm1nic" {
+  name                = local.nic_name
   location            = local.location
   resource_group_name = local.resource_group_name
 
@@ -122,8 +120,8 @@ resource "azurerm_network_interface" "VM1NIC" {
 }
 
 //Creating NSG
-resource "azurerm_network_security_group" "NSG1" {
-  name                = local.NSG_name
+resource "azurerm_network_security_group" "network-security-group" {
+  name                = local.nsg_name
   location            = local.location
   resource_group_name = local.resource_group_name
 
@@ -139,45 +137,73 @@ resource "azurerm_network_security_group" "NSG1" {
     destination_address_prefix = "*"
   }
   depends_on = [
-    azurerm_resource_group.RG3
+    azurerm_resource_group.resource-group
   ]
 }
 
 //Assigning NSG to SubnetA
-resource "azurerm_subnet_network_security_group_association" "NSG1-Association" {
+resource "azurerm_subnet_network_security_group_association" "NSGAssociation" {
   subnet_id                 = azurerm_subnet.SubnetA.id
-  network_security_group_id = azurerm_network_security_group.NSG1.id
+  network_security_group_id = azurerm_network_security_group.network-security-group.id
   depends_on = [
-    azurerm_network_security_group.NSG1,
+    azurerm_network_security_group.network-security-group,
     azurerm_subnet.SubnetA
   ]
 }
 
 //Creating VM
-resource "azurerm_windows_virtual_machine" "VM1" {
-  name                = local.VM_name
+resource "azurerm_windows_virtual_machine" "virtual-machine" {
+  name                = local.vm_name
   resource_group_name = local.resource_group_name
   location            = local.location
-  size                = local.VM_size
-  admin_username      = local.VM_username
-  admin_password      = local.VM_password
+  size                = local.vm_size
+  admin_username      = local.vm_username
+  admin_password      = local.vm_password
   network_interface_ids = [
-    azurerm_network_interface.VM1NIC.id,
+    azurerm_network_interface.vm1nic.id,
   ]
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = local.VM_storage_account_type
+    storage_account_type = local.vm_storage_account_type
   }
 
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = local.VM_sku
+    sku       = local.vm_sku
     version   = "latest"
   }
   depends_on = [
-    azurerm_network_interface.VM1NIC
+    azurerm_network_interface.vm1nic
+  ]
+}
+
+//Creating managed disk
+resource "azurerm_managed_disk" "VM1ManagedDisk" {
+  name                 = "appdisk"
+  location             = local.location
+  resource_group_name  = local.resource_group_name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "1"
+
+  tags = {
+    environment = "staging"
+  }
+  depends_on = [
+    azurerm_resource_group.resource-group
+  ]
+}
+
+//Assigning managed disk
+resource "azurerm_virtual_machine_data_disk_attachment" "azurerm_virtual_machine_data_disk_attachment" {
+  managed_disk_id    = azurerm_managed_disk.VM1ManagedDisk.id
+  virtual_machine_id = azurerm_windows_virtual_machine.virtual-machine.id
+  lun                = "0"
+  caching            = "ReadWrite"
+  depends_on = [
+    azurerm_windows_virtual_machine.virtual-machine
   ]
 }
 
@@ -186,3 +212,4 @@ resource "azurerm_windows_virtual_machine" "VM1" {
 output "SubnetA-ID" {
   value = azurerm_subnet.SubnetA.id
 }
+
