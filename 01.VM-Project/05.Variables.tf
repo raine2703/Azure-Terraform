a87662c98b0e728354b49899cfafffeea674f47e
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-03 14:06:15 +0200

    Update 05.Variables.tf

--- 01.VM-Project/05.Variables.tf
+++ 01.VM-Project/05.Variables.tf
@@ -2,7 +2,7 @@
 variable "number-of-subnets" {
     type = number
     description = "Defines number of subnets"
-    default = 2
+    default = 3
     validation {
         condition = var.number-of-subnets < 5
         error_message = "The number of subnets must be less than 5!"
@@ -11,7 +11,7 @@ variable "number-of-subnets" {
 variable "number-of-machines" {
     type = number
     description = "Defines number or virtual machines"
-    default = 2
+    default = 3
     validation {
       condition = var.number-of-machines < 5
       error_message = "The number of VMs can't  bet more than 5!"
