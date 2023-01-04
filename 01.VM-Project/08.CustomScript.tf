7d2aea81caff53ee2e09c02131be1079a7982035
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-04 13:04:29 +0200

    Update 08.CustomScript.tf

--- 01.VM-Project/08.CustomScript.tf
+++ 01.VM-Project/08.CustomScript.tf
@@ -1,7 +1,5 @@
 //Creating a custom Script extension to install web server on all VMs
 
-/*
-
 resource "azurerm_storage_account" "rnstorage2703x56" {
   name                     = "rnstorage2703x56"
   resource_group_name      = local.resource_group_name
@@ -53,5 +51,3 @@ depends_on = [
 ]
 
 }
-
-*/
