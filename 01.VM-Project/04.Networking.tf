f0ed65be779a3c2eda184b33ca268a1e4a22d11f
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-04 12:29:37 +0200

    Update 04.Networking.tf

--- 01.VM-Project/04.Networking.tf
+++ 01.VM-Project/04.Networking.tf
@@ -38,6 +38,17 @@ resource "azurerm_network_security_group" "network-security-group" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 400
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
   depends_on = [
     azurerm_resource_group.resource-group
   ]
