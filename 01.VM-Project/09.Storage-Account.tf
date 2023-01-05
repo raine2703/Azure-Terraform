30c40c21c5b659f828f124049246830f01971a84
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-05 12:42:50 +0200

    Update 09.Storage-Account.tf

--- 01.VM-Project/09.Storage-Account.tf
+++ 01.VM-Project/09.Storage-Account.tf
@@ -1,13 +1,16 @@
 //Storage account with random name and network rules to allow only connection from VMs and my IP
-/*
 
+//Creating unique identifier
 resource "random_uuid" "random-uuid" {
 }
+
+//Getting my IP
 data "http" "ip" {
   url = "https://ifconfig.me/ip"
 }
 
 resource "azurerm_storage_account" "storage-account" {
+//using lower, join and substr functions
   name = lower(join("", ["${local.storage_acc_name}", substr(random_uuid.random-uuid.result,0,8)]))  
   resource_group_name      = local.resource_group_name
   location                 = local.location
@@ -16,7 +19,8 @@ resource "azurerm_storage_account" "storage-account" {
   account_kind = "StorageV2"
   depends_on = [
     azurerm_resource_group.resource-group,
-    random_uuid.random-uuid
+    random_uuid.random-uuid,
+    azurerm_subnet.subnets
   ]
     network_rules {
     default_action             = "Deny"
@@ -30,4 +34,3 @@ resource "azurerm_storage_account" "storage-account" {
      value = azurerm_subnet.subnets.*.id
  }
 
-*/
