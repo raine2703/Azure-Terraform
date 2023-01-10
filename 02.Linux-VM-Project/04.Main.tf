3fc1f926faabd631e47a745976d6926c2ed66a80
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-10 18:00:25 +0200

    Update 04.Main.tf

--- 02.Linux-VM-Project/04.Main.tf
+++ 02.Linux-VM-Project/04.Main.tf
@@ -1,7 +1,3 @@
-//Creating Linux VM with SSH key.
-//To log in: ssh -i .\linuxkey.pem adminuser@20.223.183.94
-//Installing nginx with custom script
-
 //Creating Resource Group
 resource "azurerm_resource_group" "resource-group" {
   name     = local.resource_group_name
