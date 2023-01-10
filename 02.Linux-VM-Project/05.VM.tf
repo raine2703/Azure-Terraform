9f562c6857433b3e5cc08948a2c758c3c8591c0e
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-10 16:11:25 +0200

    Create 05.VM.tf

---
+++ 02.Linux-VM-Project/05.VM.tf
@@ -0,0 +1,38 @@
+//Creating Linux VM with SSH key.
+//To log in: ssh -i .\linuxkey.pem adminuser@20.223.183.94
+//Installing nginx with custom script
+
+//Creating Linux VM
+resource "azurerm_linux_virtual_machine" "virtual-machine" {
+  name                = local.vm_name
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  size                = local.vm_size
+  admin_username      = local.vm_username
+  //Script must be base64 encoded and value rendered
+  custom_data         = base64encode(data.template_file.cloudinitdata.rendered)
+  network_interface_ids = [
+    azurerm_network_interface.vm1nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = local.vm_username
+    public_key = tls_private_key.linuxkey.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = local.vm_storage_account_type
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+  depends_on = [
+    azurerm_network_interface.vm1nic,
+    tls_private_key.linuxkey
+  ]
+}
