1dd0ccf5f4b0fee11789392df3bb6bf5fef8c3ce
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-13 00:05:10 +0200

    Update 04.WebApp.tf

--- 03.Web-App-Project/04.WebApp.tf
+++ 03.Web-App-Project/04.WebApp.tf
@@ -1,6 +1,6 @@
 //Creating azure service plan, web app, deployment slot and enabled server logging
 
-//Creating S1 Service plan.  S1 needed to have Deployment Slots
+//Creating S1 Service plan.  Deployment slots available starting from S1 tier.
 resource "azurerm_service_plan" "ServicePlan" {
   name                = local.service_plan_name
   resource_group_name = local.resource_group_name
@@ -24,7 +24,15 @@ resource "azurerm_windows_web_app" "WebAppRn2703" {
         current_stack = "dotnet"
         dotnet_version = "v6.0"
     }
+//Testing networking options. Denying all tyraffic to WebAppRn2703
+      ip_restriction{
+        action="Deny"
+        ip_address="0.0.0.0/0"
+        name="Deny_AllTraffic"
+        priority =200 
+      }
   }
+  
 //Enabling Web server logs. Connection with shared access signature.
   logs {
     detailed_error_messages = true
@@ -33,7 +41,29 @@ resource "azurerm_windows_web_app" "WebAppRn2703" {
         retention_in_days=7   
         sas_url =  "https://${azurerm_storage_account.storage-account.name}.blob.core.windows.net/${azurerm_storage_container.logs.name}${data.azurerm_storage_account_blob_container_sas.accountsas.sas}"
     }
+  }
 }
+
+//Enabling application insights. Defining where to send logs. Multiple settings needed to be enabled and definded for monitoring to work. Requirments changes over time.
+app_settings = {
+  //"APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.appinsights.instrumentation_key
+  // "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.appinsights.connection_string
+    "MSDEPLOY_RENAME_LOCKED_FILES"                    = "1"
+    "WEBSITE_HEALTHCHECK_MAXPINGFAILURES"             = "10"
+    "ASPNETCORE_ENVIRONMENT"                          = "Development"
+    "APPINSIGHTS_INSTRUMENTATIONKEY"                  = azurerm_application_insights.appinsights.instrumentation_key
+    "APPLICATIONINSIGHTS_CONNECTION_STRING"           = azurerm_application_insights.appinsights.connection_string
+    "APPINSIGHTS_PROFILERFEATURE_VERSION"             = "1.0.0"
+    "APPINSIGHTS_SNAPSHOTFEATURE_VERSION"             = "1.0.0"
+    "ApplicationInsightsAgent_EXTENSION_VERSION"      = "~2"
+    "DiagnosticServices_EXTENSION_VERSION"            = "~3"
+    "InstrumentationEngine_EXTENSION_VERSION"         = "disabled"
+    "SnapshotDebugger_EXTENSION_VERSION"              = "disabled"
+    "XDT_MicrosoftApplicationInsights_BaseExtensions" = "disabled"
+    "XDT_MicrosoftApplicationInsights_Java"           = "1"
+    "XDT_MicrosoftApplicationInsights_Mode"           = "recommended"
+    "XDT_MicrosoftApplicationInsights_NodeJS"         = "1"
+    "XDT_MicrosoftApplicationInsights_PreemptSdk"     = "disabled"
 }
   depends_on = [
     azurerm_service_plan.ServicePlan
