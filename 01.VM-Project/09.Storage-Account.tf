78cb971f3d0d2295d8ef975c98c378dee5827fcf
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-13 03:14:53 +0200

    Update 09.Storage-Account.tf

--- 01.VM-Project/09.Storage-Account.tf
+++ 01.VM-Project/09.Storage-Account.tf
@@ -24,6 +24,7 @@ resource "azurerm_storage_account" "storage-account" {
   ]
     network_rules {
     default_action             = "Deny"
+//By default all network connections denied. Allowing my ip.
     ip_rules                   = [data.http.ip.response_body]
     virtual_network_subnet_ids = azurerm_subnet.subnets.*.id
   }
