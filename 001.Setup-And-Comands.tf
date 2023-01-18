46537af6ba8d407423d864db12758f040961cfc1
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-18 18:57:34 +0200

    Update 001.Setup-And-Comands.tf

--- 001.Setup-And-Comands.tf
+++ 001.Setup-And-Comands.tf
@@ -1,3 +1,4 @@
+
 //Terraform Set Up - Download Terraform, Add Environment variable in Windows, Choose provider and Start using Terraform!
 //Terraform as application must be registered in AAD. Also it must be granted necessary RBAC permissions on Subscription level. Contributor as example.
 
