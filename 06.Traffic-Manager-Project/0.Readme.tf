3aae6ac9d8d8fb14dcbddb0540ea86fbfeaf087e
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-21 17:44:46 +0200

    Create 0.Readme.tf

---
+++ 06.Traffic-Manager-Project/0.Readme.tf
@@ -0,0 +1,14 @@
+Project for Traffic Manager
+
+Traffic Manager = Global Load Balancing across Regions
+
+Exploring how Traffic manager routes requests to Azure web apps depending on traffic_routing_method's. 
+
+Web Apps:
+    WebApp1 created in North Europe
+    WebApp2 created in Central US
+
+Traffic Manager:
+    Note - Location not required to create resource!
+    Primaryendpoint for WebApp1
+    Secondaryendpoint for WebApp2
