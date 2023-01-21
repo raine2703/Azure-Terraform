ec7f966c1917e3da0be30e700eb6869d8678f2f4
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-21 13:07:35 +0200

    Create 09.IIS_Config.ps1

---
+++ 05.Load-Balancer-Project/09.IIS_Config.ps1
@@ -0,0 +1,6 @@
+#Custom script to install web server on virtual machines
+import-module servermanager
+add-windowsfeature web-server -includeallsubfeature
+add-windowsfeature Web-Asp-Net45
+add-windowsfeature NET-Framework-Features
+Set-Content -Path "C:\inetpub\wwwroot\Default.html" -Value "This is the server $($env:computername)"
