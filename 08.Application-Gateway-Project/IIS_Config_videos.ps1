83530539994ac437835202a71d3b06f99c482d3f
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-22 20:19:52 +0200

    Update IIS_Config_videos.ps1

--- 08.Application-Gateway-Project/IIS_Config_videos.ps1
+++ 08.Application-Gateway-Project/IIS_Config_videos.ps1
@@ -1,1 +1,7 @@
-
+#Custom script to install web server on virtual machine and define video server.
+import-module servermanager
+add-windowsfeature web-server -includeallsubfeature
+add-windowsfeature Web-Asp-Net45
+add-windowsfeature NET-Framework-Features
+New-Item -Path "C:\inetpub\wwwroot\" -Name "videos" -ItemType "directory"
+Set-Content -Path "C:\inetpub\wwwroot\videos\Default.html" -Value "This is the videos server"
