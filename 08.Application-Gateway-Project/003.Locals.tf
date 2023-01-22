54256dff3460d78d1c65668f9c07c41ae02af08d
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-22 20:18:34 +0200

    Update 003.Locals.tf

--- 08.Application-Gateway-Project/003.Locals.tf
+++ 08.Application-Gateway-Project/003.Locals.tf
@@ -1,1 +1,11 @@
+//Defining local variables
+locals {
+  resource_group_name = "RG3"
+  location = "North Europe"
+  virtual_network = {
+    name = "vnet"
+    address_space = "10.0.0.0/16"
+  }
+  role=["videos","images"]
 
+}
