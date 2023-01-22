0abd6e232b6405c239fc8863086c06df03a5988a
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-22 20:18:08 +0200

    Update 001.Provider.tf

--- 08.Application-Gateway-Project/001.Provider.tf
+++ 08.Application-Gateway-Project/001.Provider.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.37.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  subscription_id = "d030343c-fdd7-47cb-a6b7-b7027471025d"
+  tenant_id = "c20a49b8-2914-4624-a197-1e882bd3abf4"
+  client_id = "8498fe17-629b-4ad3-9d14-a6c2c37b90a9"
+  client_secret = "Yrr8Q~dHXo1wZZV-sl_gvzt61ZvxEpeIOd8.Gdo_"
+  features {}
+}
