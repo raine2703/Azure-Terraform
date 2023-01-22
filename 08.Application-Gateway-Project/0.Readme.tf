42826e276fcc001b5d1e2f2c36a027f438b7d088
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-22 20:17:16 +0200

    Update 0.Readme.tf

--- 08.Application-Gateway-Project/0.Readme.tf
+++ 08.Application-Gateway-Project/0.Readme.tf
@@ -1,1 +1,22 @@
+Projet for Application Gateway. Filtering http requests to different vms based on path.
+
+Layer 7 load balancing. Regional.
+
+Netwrorking:
+    One virtual network.
+    Two subnets - one for vm, one for APP Gateway.
+
+Virtual Machines:
+    Two Virtual machines with IIS installed via custom script.
+    Each VM has its own custom script
+    No public IP
+
+Application Gateway:
+    Requires its own subnet
+    Requires public IP iddress for Frontend configuration
+    Two backeend pools: one for videosvm, one for picturesvm
+    Routing RuleA: 
+            With Listener to frontend address on port 80 HTTP.
+            Path based routing /images/* for imagesvm
+            Path based routing /videos/* for videosvm
 
