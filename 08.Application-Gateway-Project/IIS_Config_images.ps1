21e23ad9150e7abfb24942d5454327f0caa5a01b
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-22 20:20:09 +0200

    Update IIS_Config_images.ps1

--- 08.Application-Gateway-Project/IIS_Config_images.ps1
+++ 08.Application-Gateway-Project/IIS_Config_images.ps1
@@ -1,1 +1,7 @@
-
+#Custom script to install web server on virtual machine and define images server.
+import-module servermanager
+add-windowsfeature web-server -includeallsubfeature
+add-windowsfeature Web-Asp-Net45
+add-windowsfeature NET-Framework-Features
+New-Item -Path "C:\inetpub\wwwroot\" -Name "images" -ItemType "directory"
+Set-Content -Path "C:\inetpub\wwwroot\images\Default.html" -Value "This is the images server"
