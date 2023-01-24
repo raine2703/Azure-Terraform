409fbca3fbd91121e7b7c7d5bc44313f2c7858d8
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-24 09:51:56 +0200

    Update 008.Dynamic-Blocks.tf

--- 008.Dynamic-Blocks.tf
+++ 008.Dynamic-Blocks.tf
@@ -28,17 +28,21 @@ resource "azurerm_network_security_group" "network-security-group" {
 
   //Defing NSG rule as Dynamic Block with multiple possible input variables
   dynamic "security_rule" {
+      
     for_each = local.networksecuritygroup_rules
-    content {
-        name                       = "Allow-${security_rule.value.destination_port_range}"
-        priority                   = security_rule.value.priority
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = security_rule.value.destination_port_range
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"    
+      
+        content {
+            
+            name                       = "Allow-${security_rule.value.destination_port_range}"
+            priority                   = security_rule.value.priority
+            direction                  = "Inbound"
+            access                     = "Allow"
+            protocol                   = "Tcp"
+            source_port_range          = "*"
+            destination_port_range     = security_rule.value.destination_port_range
+            source_address_prefix      = "*"
+            destination_address_prefix = "*"    
+            
       }
     }
     depends_on = [
