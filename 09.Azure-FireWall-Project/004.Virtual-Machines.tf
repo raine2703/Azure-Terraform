9e89845e1991051cfba35761f5b7cab70cc8d60c
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-25 12:59:30 +0200

    Create 004.Virtual-Machines.tf

---
+++ 09.Azure-FireWall-Project/004.Virtual-Machines.tf
@@ -0,0 +1,44 @@
+resource "azurerm_network_interface" "nic" {  
+  name                = "nic"
+  location            = local.location  
+  resource_group_name = local.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnetA.id
+    private_ip_address_allocation = "Dynamic"    
+  }
+
+  depends_on = [
+    azurerm_virtual_network.vnet
+  ]
+}
+
+
+resource "azurerm_windows_virtual_machine" "vm" {  
+  name                = "vm1"
+  resource_group_name = local.resource_group_name
+  location            = local.location 
+  size                = "Standard_D2s_v3"
+  admin_username      = "adminuser"
+  admin_password      = "Azure@123"      
+    network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+  depends_on = [
+    azurerm_virtual_network.vnet,
+    azurerm_network_interface.nic
+  ]
+}
