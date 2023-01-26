4288a12d49b17108e136e93edea1b4173690b38a
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-01-26 11:57:38 +0200

    Create 005.Netwokring.tf

---
+++ 10.Monitoring/005.Netwokring.tf
@@ -0,0 +1,42 @@
+resource "azurerm_network_interface" "nic" {  
+  name                = "Vm1Nic"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnetA.id
+    private_ip_address_allocation = "Dynamic"    
+  }
+  depends_on = [
+    azurerm_subnet.subnetA
+  ]
+}
+
+resource "azurerm_windows_virtual_machine" "vm1" {
+  name                = "Vm1"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "adminuser"
+  admin_password      = "Azure@123"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+  depends_on = [
+    azurerm_network_interface.nic,
+    azurerm_resource_group.resource-group
+  ]
+}
