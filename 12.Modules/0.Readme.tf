105d13a168a6cc6a50b48bbdb38c2520b217d691
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-02-01 01:29:01 +0200

    Create 0.Readme.tf

---
+++ 12.Modules/0.Readme.tf
@@ -0,0 +1,8 @@
+Project for Modules and Outputs
+
+Creating Networkig module and Storage account module (new Folder). 
+
+Module is like function. Can be reused several times by passing variables to them.
+
+Its also possible to retrieve values back from modules with outputs. As example using Networking module outputs to create VM.
+
