358c79985d8f8efbdc78b8b70f199de3e30aad68
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-02-08 19:18:50 +0200

    Create 1.variables.tf

---
+++ 12.Modules/modules/general/1.variables.tf
@@ -0,0 +1,10 @@
+//Defining variables that will be used to create Resource group. Variables passed from main module.
+
+variable "rg_name" {
+  type = string
+  description = "Resource group name"
+}
+variable "rg_location" {
+  type = string
+  description = "Resource group location"
+}
