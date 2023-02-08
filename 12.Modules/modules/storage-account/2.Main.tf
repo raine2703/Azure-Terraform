850b05387a43b9d4a630c29de1bdf00744d6472d
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-02-08 19:53:31 +0200

    Update 2.Main.tf

--- 12.Modules/modules/storage-account/2.Main.tf
+++ 12.Modules/modules/storage-account/2.Main.tf
@@ -1,7 +1,7 @@
 //Creating Storage account based on defined variables
 
 resource "azurerm_storage_account" "storage-account" {
-  //Log -  if false then storage account will be created once. Workaround for if/then.
+  //Logic -  if false then storage account will be created once. Workaround for if/then.
   count =var.storage2_not_required ? 0 : 1 
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
