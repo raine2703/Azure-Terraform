a375ea436ed3e530a1bdbf3a7ec7c73b47acf172
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-02-08 19:22:24 +0200

    Create 3.outputs.tf

---
+++ 12.Modules/modules/networking/3.outputs.tf
@@ -0,0 +1,9 @@
+//Defining outputs that will be used by other modules. For example - for depends_on valiadtion that resource is created.
+
+output "virtual_network" {
+  value=azurerm_virtual_network.network
+}
+
+output "subnets" {
+  value=azurerm_subnet.subnets
+}
