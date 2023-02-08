53e724a044d430e037060a208c2d1d2b167a2674
raine2703 <118910194+raine2703@users.noreply.github.com>
2023-02-08 19:23:11 +0200

    Create variables.tf

---
+++ 12.Modules/modules/storage-account/variables.tf
@@ -0,0 +1,22 @@
+//Defining variables that will be used to create Storage account. Variables passed from main module.
+
+variable "storage_account_name" {
+  type=string
+  description="This defines the name of the virtual network"
+}
+
+variable "resource_group_name" {
+  type=string
+  description="This defines the resource group name"
+}
+
+variable "location" {
+  type=string
+  description="This defines the location"
+}
+
+variable "storage2_not_required" {
+  type=bool
+  description="Is SA not required?"
+  //default = false
+}
